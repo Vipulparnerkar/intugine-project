81e3a080474a691023a8efc63363e6ba1d703bb0
Vipul <parnerkarvipul@gmail.com>
2021-01-02 19:23:30 +0530

    edited content

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -3,16 +3,6 @@ import './content.css'
 
 
 const Content = (props) =>{
-
-    // const all = props.Data.map((item,i)=>{
-        
-
-    //     if(typeof props.Data[i].scan != "undefined"){
-    //        return props.Data[i].scan.map((items,idx)=>{
-    //             return <div>{props.Data[i].scan[idx].location}</div>
-    //         })
-    //     }
-    // })
    
     const all = props.filtered.map((item,i)=>{
         
@@ -23,7 +13,7 @@ const Content = (props) =>{
             })
         }
     })
-    var dataArr = []
+    
   
     const awb = props.filtered.map((item,i)=>{
         return <p key={i}>{props.filtered[i].awbno}</p>})
@@ -31,26 +21,18 @@ const Content = (props) =>{
         return <p key={i}>{props.filtered[i].carrier}</p>})
     const Source = props.filtered.map((item,i)=>{
         return <p key={i}>{props.filtered[i].from}</p>})
-     const Destination = props.filtered.map((item,i)=>{
+    const Destination = props.filtered.map((item,i)=>{
         return <p key={i}>{props.filtered[i].to}</p>})
        
-            const start = props.filtered.map((item,i)=>{
-                return <p key={i}>{props.filtered[i].time}</p>})
-                const etd = props.filtered.map((item,i)=>{
-                    if(typeof props.filtered[i].extra_fields !="undefined" ){
-                        return <p key={i}>{props.filtered[i].extra_fields.expected_delivery_date}</p>
-                    }})
+     const start = props.filtered.map((item,i)=>{
+         return <p key={i}>{props.filtered[i].time}</p>})
+     const etd = props.filtered.map((item,i)=>{
+        if(typeof props.filtered[i].extra_fields !="undefined" ){
+             return <p key={i}>{props.filtered[i].extra_fields.expected_delivery_date}</p>
+     }})
                    
-                    const status = props.filtered.map((item,i)=>{
-                        return <p>{ props.filtered[i].current_status}</p>})
-    
- 
-   
-    
-   
-   
-     
-
+     const status = props.filtered.map((item,i)=>{
+        return <p>{ props.filtered[i].current_status}</p>})
 
 	return (
         <div className="con">
@@ -63,74 +45,41 @@ const Content = (props) =>{
                     <li key={props.Data._id}>
                     
                         {all}
-                       
-                        
-                        
+                 
                     </li>
                 </ul>
                 < img className="logob" src="FrontendAssets\warehouse.svg" alt="pic"/>
 
        </div>
 
-      
-       
-
         </div>
-
-        {/* <div className="fl"> {awb}</div>
-        <div className="fl">{Transporter} </div>
-        <div  className="fl" >{Source} </div>
-       
-        <div  className="fl">{Destination}</div>
-        <div  className="fl"> {start}</div>
-        
-        <div  className="fl">{etd} </div>
-        <div  className="fl">{status} </div> */}
         <div className="fl">
         <table className="table">
-  <thead>
-    <tr>
-    <th scope="col">AWBNO</th>
-      <th scope="col">TRANSPORTER</th>
-      <th scope="col">SOURCE</th>
-      <th scope="col">Start</th>
-      <th scope="col">ETD</th>
-      <th scope="col">Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr >
-    <td>{awb}</td>
-      <td>{Transporter}</td>
-      <td>{Source}</td>
-      <td>{start}</td>
-      <tr>{etd}</tr>
-      <td className="forstatus">{status}</td>
-    </tr>
-   
-    
-  </tbody>
-</table>
-
-
-        </div>
-       
-       
-
-
-
-
+            <thead>
+                <tr>
+                <th scope="col">AWBNO</th>
+                <th scope="col">TRANSPORTER</th>
+                <th scope="col">SOURCE</th>
+                <th scope="col">Start</th>
+                <th scope="col">ETD</th>
+                <th scope="col">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr >
+                <td>{awb}</td>
+                <td>{Transporter}</td>
+                <td>{Source}</td>
+                <td>{start}</td>
+                <tr>{etd}</tr>
+                <td className="forstatus">{status}</td>
+                </tr>
+                           
+            </tbody>
+            </table>
         </div>
 
-
-       
-		
-
-
-
-
-
-
+        </div>
 
 		);
 
